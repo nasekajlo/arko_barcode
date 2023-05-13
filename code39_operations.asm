75fccb41105680e8500aa78db9201a6011459401
pjablons <01178530@pw.edu.pl>
2023-05-13 17:08:28 +0200

    Generates * sign

--- code39_operations.asm
+++ code39_operations.asm
@@ -15,9 +15,8 @@
 #	a3 - Color of written line (0 - black (default), 1 - white)
 # return value: none
 generate_line:
-	addi sp, sp, -12
-	sw ra, 8(sp)		#push ra
-	sw s1, 4(sp)		#push s1
+	addi sp, sp, -8
+	sw ra, 4(sp)		#push ra
 	sw a2, 0(sp)		#push a2 to preserve it
 	mv s1, a0 		#preserve imgInfo for further use
 
@@ -32,33 +31,71 @@ generate_loop:
 generate_loop_exit:
 	addi a1, a1, 1
 	lw a2, 0(sp)		#pop a2
-	lw s1, 4(sp)		#pop s1
-	lw ra, 8(sp)		#pop ra
-	addi sp, sp, 12
+	lw ra, 4(sp)		#pop ra
+	addi sp, sp, 8
 	jr ra
 
-# Generates 2 pixel-wide line
-generate_thick_line:
-	addi sp, sp, -4
-	sw ra, 0(sp)
+# ========================================================================
+# gen_lines: generate n lines by calling generate_line
+# arguments:
+#	a4 - line thickness in pixels
+gen_lines:
+	addi sp, sp, -12
+	sw ra, 8(sp)
+	sw s1, 4(sp)
+	sw s0, 0(sp)
 
+gen_lines_loop:
+	beqz a4, gen_lines_exit
 	jal generate_line
-	jal generate_line
-	
-	lw ra, 0(sp)
-	addi sp, sp, 4
+	addi a4, a4, -1
+	j gen_lines_loop
+
+gen_lines_exit:
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw ra, 8(sp)
+	addi sp, sp, 12
 	jr ra
 
 
 # ========================================================================
+# arguments:
+# 	a0 - Image Info descriptor
+#	a4 - input text
+# variables used during runtime:
+#	a1 - x coordinate
+#	a2 - y coordinate
+# return value: none
 generate:
 	addi sp, sp, -4
 	sw ra, 0(sp)		#push ra
 	
 	lw a1, ImgInfo_width(a0)
 	lw a2, ImgInfo_height(a0)
+	li a3, 0 # Set color to black
+	li s2, '*' # Must start and end with *
+
+
+generate_star:
+	li s1, 9 # Code 39 has 9 characters, set iteration count
+	la s0, codes
+	addi s0, s0, 387 # position of the * code
+
+generate_star_loop:
+	lb a4, (s0)
+	jal gen_lines
 	
-	jal generate_thick_line
+	beqz s1, generate_exit
+	addi s0, s0, 1
+	addi s1, s1, -1
+	
+	# Invert color
+	mv t0, a3
+	addi a3, a3, 1
+	rem a3, a3, t0
+
+	j generate_star_loop
 
 generate_exit:
 	lw ra, 0(sp)		#pop ra
