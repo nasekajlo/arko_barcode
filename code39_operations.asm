c7218863cbc6960e6355ed9b43947672e7b316f4
snasekaj <sofianasekajlo4@gmail.com>
2023-05-19 13:46:44 +0200

    нихрена не работает, но я держусь

--- code39_operations.asm
+++ code39_operations.asm
@@ -15,7 +15,9 @@
 #	a1 - x coordinate
 #	a2 - y coordinate
 #	a3 - Color of written line (0 - black (default), 1 - white)
-# return value: none
+#	a4 - Space to be filled
+# return value:
+#	a4 - Decoded string
 write_line:
 	#jal read_bmp
 	addi sp, sp, -4
@@ -23,50 +25,45 @@ write_line:
 
 	lw a1, ImgInfo_width(a0)
 	lw a2, ImgInfo_height(a0)
-
 	la s0, codes		#pointer on codes
 	addi sp, sp, -8
 	sw ra, 4(sp)		#push ra
 	sw a2, 0(sp)		#push a2 to preserve it
 	mv s1, a0 		#preserve imgInfo for further use
-	lb a4, 12		#pixel number in checking word
-	lb a5, 0		#bit
-	lw a6, 0		#store the symbol in binary 
-	lb a7, 2413		#star's code
+	li s2, 12		#pixel number in checking word
+	li a5, 0		#bit
+	li a6, 0		#store the symbol in binary 
+	li a7, 2413		#star's code
 	
 skip_start_spaces:
 	addi a1, a1, 1
 	beq a3, zero, skip_start_spaces
 	addi a1, a1, 13
-	b count_bar_loop
 	
 
 count_bar_loop:
 	jal get_pixel
-	sll a5, a3, a4		#color * 2^11
+	sll a5, a3, s2		#color * 2^11
 	add a6, a6, a5		#calculate symbol code in 10
-	beqz a4, find_symbol_loop
-	addi a1, a1, 1
-	addi a4, a4, -1 
+	beqz s2, find_symbol_loop
+	addi a1, a1, 1		#go to the next pixel
+	addi s2, s2, -1 	#increase position in bin
 	b count_bar_loop
 
 find_symbol_loop:		
-	lb t1, 0(s0)
-	lb a4, 12
+	lb t1, 0(s0)		#first synmol in codes
+	li s2, 12		#reload s2 by 12
 	beq t1, a6, write_symbol #symbol found	
 	beq a6, a7, count_bar_loop_exit		#last star
 	addi s0, s0, 2		#go throw element and its code in ascii
-	lw a6, 0
+	lw a6, 0		
 	b find_symbol_loop
 	
 write_symbol:
 	addi a1, a1, 1		#skip one space between symbols
 	lw a6, 0
-	addi s0, s0, 1
-	lb t1, 0(s0)
-	li a7, 4
-	mv a0, t1
-	ecall
+	addi s0, s0, 1		#take code of symbol
+	sb s0, 0(a4)		#load this symbol in a4
 	b count_bar_loop
 	
 count_bar_loop_exit:
--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@
 
 	.data
 
-imgInfo: 	.space	56	# image descriptor
+imgInfo: 	.space	28	# image descriptor
 
 	.align 2		# word boundary alignment
 dummy:		.space  2
@@ -17,12 +17,17 @@ ofname: 	.asciz  "result.bmp"
 
 input:		.space 	19 # Supports up to 19 characters
 prompt:		.asciz 	"\n(Must start and end with '*' to be valid)\nText: "
-
+	.align 0
+space:		.space 	100
 
 	.text
 	
 main:
-
+	la a4, space
+	#li t1, '9'
+	#sb t1, 0(a4)
+	#sw zero, 1(a4)
+	
 
 # initialize image descriptor
 	la a0, imgInfo 
@@ -34,14 +39,15 @@ main:
 	sw t0, ImgInfo_imdat(a0)
 	jal read_bmp
 	bnez a0, main_failure
-
+	
 	la a0, imgInfo
 	jal write_line
 	
-	la a0, imgInfo
-	la t0, ofname
-	sw t0, ImgInfo_fname(a0)
-	jal save_bmp
+	
+	la a0, space
+	li a7, 4
+	ecall
+	
 
 main_failure:
 	li a7, 10
